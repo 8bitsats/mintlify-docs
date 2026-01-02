5aed4ea4c15f3ab0ed0b87c15bdb416999313820
8bitsats <cheshireterminal@gmail.com>
2026-01-01 20:16:36 -0500

    sffssffs

--- worker.js
+++ worker.js
@@ -4,30 +4,23 @@ addEventListener("fetch", (event) => {
 
 async function handleRequest(request) {
   try {
-    const urlObject = new URL(request.url);
-    // If the request is to the docs subdirectory
-    if (/^\/docs/.test(urlObject.pathname)) {
-      // Then Proxy to Mintlify
-      const DOCS_URL = "funpump.mintlify.dev";
-      const CUSTOM_URL = "docs.funpump.ai";
+    // Proxy all requests to Mintlify
+    const DOCS_URL = "funpump.mintlify.dev";
+    const CUSTOM_URL = "docs.funpump.ai";
 
-      let url = new URL(request.url);
-      url.hostname = DOCS_URL;
+    let url = new URL(request.url);
+    url.hostname = DOCS_URL;
 
-      let proxyRequest = new Request(url, request);
+    let proxyRequest = new Request(url, request);
 
-      proxyRequest.headers.set("Host", DOCS_URL);
-      proxyRequest.headers.set("X-Forwarded-Host", CUSTOM_URL);
-      proxyRequest.headers.set("X-Forwarded-Proto", "https");
+    proxyRequest.headers.set("Host", DOCS_URL);
+    proxyRequest.headers.set("X-Forwarded-Host", CUSTOM_URL);
+    proxyRequest.headers.set("X-Forwarded-Proto", "https");
 
-      return await fetch(proxyRequest);
-    }
+    return await fetch(proxyRequest);
   } catch (error) {
     // if error occurs, return the regular request
     return await fetch(request);
   }
-
-  // If path doesn't match /docs, return the regular request
-  return await fetch(request);
 }
 
